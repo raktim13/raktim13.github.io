50ab5be3cad38cac01cb4ae623ad70cdd0e3172c
Raktim Debnath <raktimdebnath105c@gmail.com>
2026-07-12 17:12:21 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,68 @@
+const myNotes = {
+    "Undergrad": {
+        "Sem 1": { }
+        "Sem 2": { }
+        "Sem 3": { }
+        "Sem 4": { }
+        "Sem 5": { }
+        "Sem 6": { }
+    },
+    "MMath": {
+
+        "Sem 1": {
+          "Algebra I": { 
+            }
+          "Ananlysis of Several Variables": {
+            }
+          "Linear Algebra": {
+            }
+          "Measure Theory": {
+            }
+          "Topology I": {
+            }
+        }
+
+        "Sem 2": {
+          "Algebra II": {
+            }
+          "Complex Analysis": {
+            }
+          "Functional Analysis": {
+            }
+          "Number Theory": {
+            }
+          "Topology II": {
+            }
+        }
+
+        "Sem 3": {
+          "Commutative Algebra I": {
+            }
+          "Differential Geometry I": {
+            }
+          "Probability Theory": {
+            }
+          "Riemann Surfaces": {
+            }
+          "Topology III": {
+            }
+        }
+
+      
+        "Sem 4": {
+            "Algebraicic Geometry": {
+                "Assignment 1": "",  
+            }
+          "Partial Differential Equations": {  
+            }
+          "Rational Homotopy Theory": {  
+            }
+          "Symplectic Geometry": {
+                "Assignment 1": ,
+            }
+            "Teichmuller Theory": { 
+            }
+        }
+    },
+    "JRF": {}
+};
