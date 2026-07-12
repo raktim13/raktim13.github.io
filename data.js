146fc9ed6aa880e14e906262541c599423a2f584
Raktim Debnath <raktimdebnath105c@gmail.com>
2026-07-12 17:30:28 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,67 +1,44 @@
 const myNotes = {
     "Undergrad": {
-        "Sem 1": { }
-        "Sem 2": { }
-        "Sem 3": { }
-        "Sem 4": { }
-        "Sem 5": { }
-        "Sem 6": { }
+        "Sem 1": {},
+        "Sem 2": {},
+        "Sem 3": {},
+        "Sem 4": {},
+        "Sem 5": {},
+        "Sem 6": {}
     },
     "MMath": {
-
         "Sem 1": {
-          "Algebra I": { 
-            }
-          "Ananlysis of Several Variables": {
-            }
-          "Linear Algebra": {
-            }
-          "Measure Theory": {
-            }
-          "Topology I": {
-            }
-        }
-
+            "Algebra I": {},
+            "Analysis of Several Variables": {},
+            "Linear Algebra": {},
+            "Measure Theory": {},
+            "Topology I": {}
+        },
         "Sem 2": {
-          "Algebra II": {
-            }
-          "Complex Analysis": {
-            }
-          "Functional Analysis": {
-            }
-          "Number Theory": {
-            }
-          "Topology II": {
-            }
-        }
-
+            "Algebra II": {},
+            "Complex Analysis": {},
+            "Functional Analysis": {},
+            "Number Theory": {},
+            "Topology II": {}
+        },
         "Sem 3": {
-          "Commutative Algebra I": {
-            }
-          "Differential Geometry I": {
-            }
-          "Probability Theory": {
-            }
-          "Riemann Surfaces": {
-            }
-          "Topology III": {
-            }
-        }
-
-      
+            "Commutative Algebra I": {},
+            "Differential Geometry I": {},
+            "Probability Theory": {},
+            "Riemann Surfaces": {},
+            "Topology III": {}
+        },
         "Sem 4": {
-            "Algebraicic Geometry": {
-                "Assignment 1": "",  
-            }
-          "Partial Differential Equations": {  
-            }
-          "Rational Homotopy Theory": {  
-            }
-          "Symplectic Geometry": {
-                "Assignment 1": ,
-            }
-            "Teichmuller Theory": { 
-            }
+            "Algebraic Geometry": {
+                "Assignment 1": "assignment1_ag.pdf"
+            },
+            "Partial Differential Equations": {},
+            "Rational Homotopy Theory": {},
+            "Symplectic Geometry": {
+                "Assignment 1": "assignment1_sg.pdf"
+            },
+            "Teichmuller Theory": {}
         }
     },
     "JRF": {}
